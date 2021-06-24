6fd6af7dd38517ded1000a241552567ec80ba213
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2021-06-24 00:47:47 -0300

    feat: update theme palette

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,15 @@
 export const defaultTheme = {
   palette: {
+    main: "#935afd",
+    secondary: "#e559f9",
     background: "#f8f8f8",
+    backgroundLight: "#fefefe",
     textMain: "#29292e",
     textMainLight: "#737380",
     textLight: "#fff",
     textLighter: "#f8f8f8",
+    borderLight: "#e2e2e2",
     border: "#a8a8b3",
     googleRed: "#ea4335",
-    main: "#935afd",
   },
 };
